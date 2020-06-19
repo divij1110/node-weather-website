0198caec48020d91e2cd8787f3e967775ab180dc
Divij Sharma <f20180751@pilani.bits-pilani.ac.in>
2020-06-19 15:37:29 +0530

    Add more weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find lcation.', undefined)
         } else {
-            callback(undefined, 'It is currently ' + response.body.current.temperature + ' degrees out. It feels like '+ response.body.current.feelslike + ' degrees out.')
+            callback(undefined, 'It is currently ' + response.body.current.temperature + ' degrees out. It feels like '+ response.body.current.feelslike + ' degrees out. The humidity is '+response.body.current.humidity+'. The weather is '+response.body.current.weather_descriptions+'.')
         }
 
     })
